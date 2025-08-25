643967f80cb799a828052bce347182aa1bb8f973
Anonnya01 <anonnya1999@gmail.com>
2025-08-26 01:32:38 +0600

    Cart value and functionality added

--- script/index.js
+++ script/index.js
@@ -1,1 +1,34 @@
-console.log(document);
+// -----function for getting value-------//
+// -----part-4----13.30-----//
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+document.getElementById("btn-k1").addEventListener("click", function () {
+  const cardTitle = getElement("title-k1").innerText;
+  const cardPrice = getElement("price-k1").innerText;
+
+  const getTotalPrice = getElement("total-price").innerText;
+
+  const currentPrice = Number(cardPrice) + Number(getTotalPrice);
+
+  getElement("total-price").innerText = currentPrice.toFixed(2);
+
+  //------update cart container-----------//
+
+  const cartCont = getElement("cart-container");
+
+  const newCart = document.createElement("div");
+  newCart.innerHTML = `
+ <div class="flex items-center justify-around bg-gray-200 rounded mb-3">
+                  <img src="assets/kitchen-1.png" class="w-15" alt="">
+                  <div class="">
+                    <h2 class="font-bold">${cardTitle}</h2>
+                    <h2>${cardPrice} TK</h2>
+                  </div>
+                </div>
+ 
+ `;
+
+  cartCont.append(newCart);
+});
---
+++ script/practice.js
@@ -0,0 +1,48 @@
+// ------alert with function----//
+// --------part-3-------//
+// -------20.55-------//
+
+// function massage() {
+
+//     alert('product clicked')
+// }
+
+// document.getElementById("btn-k1")
+// .addEventListener('click',massage)
+
+// ---------using class----//
+// const itemsTitle=document.getElementsByClassName("card-title")
+// console.log(itemsTitle);
+
+
+// -----------querySelector------//
+// function
+// --------part-3-------//
+// -------31.12-------//
+
+// function removeSpc(str) {
+//    return str.replaceAll(" ","").toUpperCase()
+// }
+
+// const itemsTitle=document.querySelectorAll(".card-title")
+// console.log(itemsTitle);
+
+// for(let title of itemsTitle){
+//    title.innerHTML =`<span class='text-purple-500'>${removeSpc(title.innerText)}</span>`
+//      title.style.backgroundColor= "pink";
+//   title.classList.add("hidden")
+//   title.classList.remove("hidden")
+// }
+
+
+// ---------img manipulation------//
+// const image = document.getElementsByTagName("img")
+// console.log(image);
+// const image = document.getElementsByTagName("img")
+// console.log(image);
+// for(let img of image){
+//   img.src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTzO8D_a-4KQvLMrJtyfLhqTZJjwWbhXV373XTI_fngfqCAdfddotgnkzlypgMstlgHsybk_n5SZ4sGNB6_oi6_8ALPWGgxrWtODaXWkoOU"
+// }
+
+
+
